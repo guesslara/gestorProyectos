513f556ad80b7cdf8fa6e0e3f23904515201fa71
guesslara <gerardolara1984@gmail.com>
2016-10-23 23:53:11 -0500

    Cambios efectuados para poder incluir el nuevo layout

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function () {
+    var pstyle = 'border: 1px solid #dfdfdf; padding: 5px;';
+    $('#layout').w2layout({
+        name: 'layout',
+        padding: 3,
+        panels: [
+            { type: 'left', size: 200, resizable: true, style: pstyle, content: 'left' },
+            { type: 'main', style: pstyle, content: 'main' }
+        ]
+    });
+});
