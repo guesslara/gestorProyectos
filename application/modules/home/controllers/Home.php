8d4bd8160bf12afaba7dbef3f42aee1f88f6bcc2
guesslara <gerardolara1984@gmail.com>
2017-03-21 21:18:25 -0600

    Comentarios en el controlador Home.php

--- application/modules/home/controllers/Home.php
+++ application/modules/home/controllers/Home.php
@@ -3,11 +3,12 @@
 	/**
 	* 
 	*/
-	include "libs/tpl.lib.php";
+	include "libs/tpl.lib.php";//libreria para pintar el template
 
 	class Home extends MY_Controller{
-
+		//directorio de vistas
 		private $dirVistas="./application/modules/home/views";
+		//variable para asignar los valores del template
 		private $tpl="";
 
 		function __construct()
@@ -16,7 +17,7 @@
 			$this->tpl = new Template($this->dirVistas);
 		}
 
-		function index(){
+		function index(){//funcion inicial del controlador
 			$this->tpl->set_filenames(array('Home' => 'default'));
             $this->tpl->assign_vars(array(
                 'TEST' => "Bienvenido a Codeigniter HMVC"
@@ -24,7 +25,7 @@
             $this->tpl->pparse('Home');
 		}
 
-		function test(){
+		function test(){//funcion de test
 			$this->tpl->set_filenames(array('Home' => 'test'));
             $this->tpl->assign_vars(array(
                 'TEST' => "Esta es una funcion TEST"
