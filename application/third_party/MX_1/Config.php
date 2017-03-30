e70f6467449c2364ece76647acc2d89c8bfb41b2
guesslara <gerardolara1984@gmail.com>
2017-03-30 00:10:04 -0600

    Se agrega la funcionalidad del login de usuarios

--- application/third_party/MX_1/Config.php
+++ application/third_party/MX_1/Config.php
@@ -1,4 +1,6 @@
-<?php (defined('BASEPATH')) OR exit('No direct script access allowed');
+<?php
+
+(defined('BASEPATH')) OR exit('No direct script access allowed');
 
 /**
  * Modular Extensions - HMVC
@@ -12,8 +14,8 @@
  *
  * Install this file as application/third_party/MX/Config.php
  *
- * @copyright	Copyright (c) 2015 Wiredesignz
- * @version 	5.5
+ * @copyright	Copyright (c) 2011 Wiredesignz
+ * @version 	5.4
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,47 +34,43 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- **/
-class MX_Config extends CI_Config 
-{	
-	public function load($file = '', $use_sections = FALSE, $fail_gracefully = FALSE, $_module = '') 
-	{
-		if (in_array($file, $this->is_loaded, TRUE)) return $this->item($file);
+ * */
+class MX_Config extends CI_Config
+{
 
-		$_module OR $_module = CI::$APP->router->fetch_module();
-		list($path, $file) = Modules::find($file, $_module, 'config/');
-		
-		if ($path === FALSE)
-		{
-			parent::load($file, $use_sections, $fail_gracefully);					
-			return $this->item($file);
-		}  
-		
-		if ($config = Modules::load_file($file, $path, 'config'))
-		{
-			/* reference to the config array */
-			$current_config =& $this->config;
+    public function load($file = 'config', $use_sections = FALSE, $fail_gracefully = FALSE, $_module = '')
+    {
 
-			if ($use_sections === TRUE)	
-			{
-				if (isset($current_config[$file])) 
-				{
-					$current_config[$file] = array_merge($current_config[$file], $config);
-				} 
-				else 
-				{
-					$current_config[$file] = $config;
-				}
-				
-			} 
-			else 
-			{
-				$current_config = array_merge($current_config, $config);
-			}
+        if (in_array($file, $this->is_loaded, TRUE))
+            return $this->item($file);
 
-			$this->is_loaded[] = $file;
-			unset($config);
-			return $this->item($file);
-		}
-	}
+        $_module OR $_module = CI::$APP->router->fetch_module();
+        list($path, $file) = Modules::find($file, $_module, 'config/');
+
+        if ($path === FALSE) {
+            parent::load($file, $use_sections, $fail_gracefully);
+            return $this->item($file);
+        }
+
+        if ($config = Modules::load_file($file, $path, 'config')) {
+
+            /* reference to the config array */
+            $current_config = & $this->config;
+
+            if ($use_sections === TRUE) {
+
+                if (isset($current_config[$file])) {
+                    $current_config[$file] = array_merge($current_config[$file], $config);
+                } else {
+                    $current_config[$file] = $config;
+                }
+            } else {
+                $current_config = array_merge($current_config, $config);
+            }
+            $this->is_loaded[] = $file;
+            unset($config);
+            return $this->item($file);
+        }
+    }
+
 }
