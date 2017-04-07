0b40683ac19eaef39dcfd12854969937a2034632
guesslara <gerardolara1984@gmail.com>
2017-04-07 00:45:02 -0500

    Se agrega el modulo principal desde el cual se cargara la GUI de proyectos

--- application/modules/login/controllers/Login.php
+++ application/modules/login/controllers/Login.php
@@ -34,7 +34,7 @@
 					$this->load->view('login_view',$data);
 					break;
 				case '0':
-					redirect(site_url().'proyectos');
+					redirect(site_url().'app');
 					break;
 				case '1':
 					redirect(site_url().'editor');
@@ -55,7 +55,6 @@
 				if($this->form_validation->run() == FALSE){
 					$this->index();
 				}else{
-					echo "Se valida el usuario";
 					$username = $this->input->post('username');
 					$password = sha1($this->input->post('password'));
 					$check_user = $this->login_model->login_user($username,$password);
